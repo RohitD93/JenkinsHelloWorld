2ac98ce0d1759a2e372e199cb9713a6f93408874
RohitD93 <105128053+RohitD93@users.noreply.github.com>
2022-05-08 01:29:42 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
  public static void main(String[]args){
-  System.out.println("------*Hello World & Welcome to jenkins training******----");
+  System.out.println("------*Made Some Changes******----");
  }
 }
 
